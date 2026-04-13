112a72018b388f33e7f445a38180791a6091f2cb
Rhaffael_Trindade <rhaffaeltrindade31@gmail.com>
2026-04-13 06:59:23 -0300

    Versão final do desafio

--- Challenge 70.py
+++ Challenge 70.py
@@ -1,13 +1,23 @@
+soma = totmil = menor = cont = 0 
+barato = ''
 print('=-='*10)
 print('Loja super Baratão')
 print('=-='*10)
 
 while True:
     nomep = str(input('Qual o nome do produto? '))
-    preco = int(input('Qual o preço do produto? '))
+    preco = float(input('Qual o preço do produto? '))
+    soma += preco
+    cont += 1
+    if cont == 1 or preco < menor:
+        menor = preco
+        barato = nomep
+   
+    if preco > 1000:
+        totmil += 1
 
     while True:
-        saida = str(input('Deseja continuar? [S/N] ')).upper().strip()
+        saida = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
         if saida in ('S', 'N'):
             break
         else:
@@ -15,6 +25,11 @@ while True:
 
     if saida == 'N':
         break
+print('FIM DO PROGRAMA')
+print(f'O total da compra foi {soma}')
+print(f'Teve {totmil} produto acima de 1000 reias')
+print(f'O produto mais barato foi {barato} que custa R$ {menor:.2f}')
+
 
 
 
